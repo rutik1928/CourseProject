228e4b7cde3db197e7b23fa87f6f6e19dec54dd4
Evgenii <rutevgenii2@gmail.com>
2025-11-03 21:58:25 +0300

    feat: add routes to server.js

--- server/src/server.js
+++ server/src/server.js
@@ -4,3 +4,14 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`✅ Сервер запущен: http://localhost:${PORT}`);
 });
+
+
+// Маршруты
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/ads', require('./routes/ads'));
+app.use('/api/comments', require('./routes/comments'));
+
+// Главная страница
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
